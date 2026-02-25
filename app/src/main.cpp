17298ef89f007a78532d1208daa44e7116ad8850
Bannirui <Bannirui@outlook.com>
2026-02-25 21:49:12 +0800

    fix directory

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,11 +2,11 @@
 // Created by dingrui on 2/1/26.
 //
 
+#include "x.h"
+
 #include <QApplication>
 
 #include "widget.h"
-
-#include "../x_video_player/src/x/x_log.h"
 
 int main(int argc, char *argv[]) {
     XLog::Init();
